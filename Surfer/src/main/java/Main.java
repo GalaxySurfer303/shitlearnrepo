8109091bf147fcdd0a297358322c0dbebe821fdd
GalaxySurfer303 <oldsamo1980@gmail.com>
2024-09-18 17:49:55 +0200

    Add files via upload

---
+++ Surfer/src/main/java/Main.java
@@ -0,0 +1,37 @@
+/*
+public class Main {
+    public static void main(String[] args) {
+        Surfer galaxy_Surfer = new Surfer("Galaxy Surfer");
+        galaxy_Surfer.surf();
+    }
+}
+
+class Surfer {
+    String name;
+
+    public Surfer(String name) {
+        this.name = name;
+    }
+
+    public void surf() {
+        System.out.println(name + " is surfing through the galaxy!");
+    }
+}
+
+ */
+
+class Main {
+    public static void main(String args[]) {
+        int myVar1;
+        int myVar2;
+
+        myVar1 = 1024;
+
+        System.out.println("myVar1 zawiera " + myVar1);
+
+        myVar2 = myVar1 / 2;
+
+        System.out.println("myVar1 zawiera myVar2 /2: ");
+        System.out.println(myVar2);
+    }
+}
